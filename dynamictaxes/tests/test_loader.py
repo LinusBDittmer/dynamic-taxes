56872040005815ed8f93d8e1d81ec5533e44cfe8
Linus Bjarne Dittmer <iu254@stud.uni-heidelberg.de>
2023-05-30 19:29:44 +0200

    Some changes

--- dynamictaxes/tests/test_loader.py
+++ dynamictaxes/tests/test_loader.py
@@ -14,21 +14,23 @@ class TestLoader(unittest.TestCase):
         dt.init_configs()
         self.loader = dt.Loader()
         self.loader2 = dt.Loader()
+        self.content = __file__[:__file__.rfind("/")+1] + "content.json"
+        self.content2 = __file__[:__file__.rfind("/")+1] + "content_temp.json"
 
     def tearDown(self):
-        subprocess.run(["rm", "-f", "content_temp.json"])
+        subprocess.run(["rm", "-f", self.content2])
         del self.loader
 
     def test_load_json(self):
         self.loader.reset()
-        self.loader.load_from_json("content.json")
+        self.loader.load_from_json(self.content)
         self.assertEqual(len(self.loader.ta_spectrum.esa_spectra), 100)
 
     def test_write_json(self):
         self.loader.reset()
-        self.loader.load_from_json("content.json")
-        self.loader.save_to_json("content_temp.json")
-        self.loader2.load_from_json("content_temp.json")
+        self.loader.load_from_json(self.content)
+        self.loader.save_to_json(self.content2)
+        self.loader2.load_from_json(self.content2)
         self.assertTrue(self.loader.ta_spectrum == self.loader2.ta_spectrum)
 
 
