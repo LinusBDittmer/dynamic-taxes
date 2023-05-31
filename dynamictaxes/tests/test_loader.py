e1c0b13190811250581c4acb7251fbaf2b6b8d8d
Linus Bjarne Dittmer <iu254@stud.uni-heidelberg.de>
2023-05-31 12:57:35 +0200

    Fixed unittests

--- dynamictaxes/tests/test_loader.py
+++ dynamictaxes/tests/test_loader.py
@@ -13,24 +13,17 @@ class TestLoader(unittest.TestCase):
     def setUp(self):
         dt.init_configs()
         self.loader = dt.Loader()
-        self.loader2 = dt.Loader()
-        self.content = __file__[:__file__.rfind("/")+1] + "content.json"
-        self.content2 = __file__[:__file__.rfind("/")+1] + "content_temp.json"
 
     def tearDown(self):
-        subprocess.run(["rm", "-f", self.content2])
         del self.loader
 
-    def test_load_json(self):
-        self.loader.reset()
-        self.loader.load_from_json(self.content)
-        self.assertEqual(len(self.loader.ta_spectrum.esa_spectra), 100)
-
-    def test_write_json(self):
-        self.loader.reset()
-        self.loader.load_from_json(self.content)
-        self.loader.save_to_json(self.content2)
-        self.loader2.load_from_json(self.content2)
-        self.assertTrue(self.loader.ta_spectrum == self.loader2.ta_spectrum)
-
+    def test_attributes(self):
+        methods = ["reset", "load_from_dir", "load_from_json", "save_to_json"]
+        variables = ["ta_spectrum"]
+        for method in methods:
+            self.assertTrue(hasattr(self.loader, method))
+            self.assertTrue(callable(getattr(self.loader, method)))
+        for variable in variables:
+            self.assertTrue(hasattr(self.loader, variable))
+            self.assertTrue(not callable(getattr(self.loader, variable)))
 
