64b7a0d304d1d27d0912cc19193fb89c1f08b8f3
Linus Bjarne Dittmer <iu254@stud.uni-heidelberg.de>
2023-06-05 14:29:42 +0200

    Fixed installer

--- dynamictaxes/installer.sh
+++ dynamictaxes/installer.sh
@@ -5,17 +5,45 @@ then
 	echo "No installation path specified"
 	exit 69
 fi
+
+echo "Installation path found."
+
 if [ -z "$2" ]
 then
 	echo "No conda environment specified"
 	exit 420
 fi
 
+echo "Conda environment specified."
+
 CONDA_ENV=$2
 INSTALL_PATH=$(realpath $1)
 MAIN_PATH=$INSTALL_PATH/dynamictaxes/main.py
 
+ANACONDA3=~/anaconda3/
+MINICONDA3=~/miniconda3/
+
+if [ -d "$ANACONDA3" ]
+then
+	CONDAPATH=$ANACONDA3
+else
+	if [ -d "$MINICONDA3" ]
+	then
+		CONDAPATH=$MINICONDA3
+	fi
+fi
+
+if [ -z "$CONDAPATH" ]
+then
+	echo "No conda installation found"
+	exit 31
+fi
+
+echo "Found conda installation at $CONDAPATH"
+
+
 cd $INSTALL_PATH
+source $CONDAPATH/etc/profile.d/conda.sh
 git init .
 git remote add origin https://github.com/LinusBDittmer/dynamic-taxes.git
 git pull origin master
@@ -27,4 +55,6 @@ echo -e "alias dynamic-taxes='python $MAIN_PATH'" >> ~/.bashrc
 echo -e "alias dt='python $MAIN_PATH'" >> ~/.bashrc
 echo -e "alias dyntax='python $MAIN_PATH'" >> ~/.bashrc
 echo -e "# End dynamic taxes" >> ~/.bashrc
+
+echo "Installation complete."
 
